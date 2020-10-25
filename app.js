cd767b46535b927eb9823c47752d4305de571881
Daniel Pöhle <daniel.poehle@deutschebahn.com>
2020-10-25 20:04:53 +0100

    add input form for template and generate from this template

--- app.js
+++ app.js
@@ -49,62 +49,44 @@
   
       maList.generateDOCX = function(){
         //console.log(maList.items);
-        var loadFile = function(url,callback) {
-          PizZipUtils.getBinaryContent(url,callback);
+        var docs = document.getElementById('template');
+        var reader = new FileReader();
+        if (docs.files.length === 0) {
+           alert("No files selected")
         }
-        loadFile("https://github.com/danielpoehle/doorplatewizard/blob/main/docx/doorplate_fpl.docx",function(error,content){
-          if (error) { throw error };
+        reader.readAsBinaryString(docs.files.item(0));
 
-          // The error object contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
-          function replaceErrors(key, value) {
-              if (value instanceof Error) {
-                  return Object.getOwnPropertyNames(value).reduce(function(error, key) {
-                      error[key] = value[key];
-                      return error;
-                  }, {});
-              }
-              return value;
-          }
+        reader.onerror = function (evt) {
+           console.log("error reading file", evt);
+           alert("error reading file" + evt)
+        }
+        reader.onload = function (evt) {
+           const content = evt.target.result;
+           var zip = new PizZip(content);
+           var doc;
+           try {
+               doc=new window.docxtemplater(zip);
+           } catch(error) {
+               // Catch compilation errors (errors caused by the compilation of the template : misplaced tags)
+               errorHandler(error);
+           }
 
-          function errorHandler(error) {
-              console.log(JSON.stringify({error: error}, replaceErrors));
+           doc.setData(maList.items);
+           try {
+               // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+               doc.render();
+           }
+           catch (error) {
+               // Catch rendering errors (errors relating to the rendering of the template : angularParser throws an error)
+               errorHandler(error);
+           }
 
-              if (error.properties && error.properties.errors instanceof Array) {
-                  const errorMessages = error.properties.errors.map(function (error) {
-                      return error.properties.explanation;
-                  }).join("\n");
-                  console.log('errorMessages', errorMessages);
-                  // errorMessages is a humanly readable message looking like this :
-                  // 'The tag beginning with "foobar" is unopened'
-              }
-              throw error;
-          }
-
-          var zip = new PizZip(content);
-          var doc;
-          try {
-              doc=new window.docxtemplater(zip);
-          } catch(error) {
-              // Catch compilation errors (errors caused by the compilation of the template : misplaced tags)
-              errorHandler(error);
-          }
-
-          doc.setData(maList.items);
-          try {
-              // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-              doc.render();
-          }
-          catch (error) {
-              // Catch rendering errors (errors relating to the rendering of the template : angularParser throws an error)
-              errorHandler(error);
-          }
-
-          var out=doc.getZip().generate({
+           var out=doc.getZip().generate({
               type:"blob",
               mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-          }) //Output the document using Data-URI
-          saveAs(out,"output.docx")
-      });
+           }) //Output the document using Data-URI
+           saveAs(out,"output.docx")
+        };        
       };
   
       maList.editZNr = function(itemID, znr){
